ec670aa2ef5e0c3de6f7f5512e6c95d67b958c27
Todd Wang <1103764204@qq.com>
2024-10-13 18:16:52 +0800

    fix: bin command, no compare result leave empty file

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-import { NO_READ_AUTH_MSG } from 'constant/errorMessage'
-import { INVALID_PATH_CODE } from './constant/errCode'
+import {
+  COMP_FILE_PATH_REQUIRED_MSG,
+  NO_READ_AUTH_MSG,
+  OUT_FILE_PATH_REQUIRED_MSG,
+  REF_FILE_PATH_REQUIRED_MSG
+} from 'constant/errorMessage'
+import {
+  COMP_FILE_PATH_REQUIRED,
+  INVALID_PATH_CODE,
+  OUT_FILE_PATH_REQUIRED,
+  REF_FILE_PATH_REQUIRED
+} from './constant/errCode'
 import { isValidPath } from './util/file'
 import { throwErrorWithCode } from 'util/error'
 import { compareFileWrite2File } from 'core'
@@ -10,6 +20,30 @@ export const compareJSON2File = async (
   outputPath: string,
   chunkSize?: number
 ) => {
+  if (!referenceFilePath) {
+    throwErrorWithCode(
+      REF_FILE_PATH_REQUIRED,
+      REF_FILE_PATH_REQUIRED_MSG,
+      referenceFilePath
+    )
+  }
+
+  if (!comparedFilePath) {
+    throwErrorWithCode(
+      COMP_FILE_PATH_REQUIRED,
+      COMP_FILE_PATH_REQUIRED_MSG,
+      comparedFilePath
+    )
+  }
+
+  if (!outputPath) {
+    throwErrorWithCode(
+      OUT_FILE_PATH_REQUIRED,
+      OUT_FILE_PATH_REQUIRED_MSG,
+      outputPath
+    )
+  }
+
   Promise.all([
     isValidPath(referenceFilePath),
     isValidPath(comparedFilePath)
